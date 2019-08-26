c207bec736c8a4a3d22bba76460eb5607889077f
Rodion <rodion77777@gmail.com>
2019-08-26 22:48:55 +0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        System.out.println("Введите адрес .txt файла в следующем формате \"D:/Desktop/doc1.txt\" : ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите в консоли адрес .txt файла в следующем формате D:/Desktop/doc1.txt " +
+                "или в формате имя_файла.txt, если файл находится в корневом каталоге проекта: ");
+
         BufferedReader bufReader = new BufferedReader(new FileReader(reader.readLine()));
-        ArrayList<String> listOfLines = new ArrayList<String>();
+        ArrayList<String> listOfLines = new ArrayList<>();
         String line = bufReader.readLine();
-        String result = "";
+
 
         while (line != null)
         {
             String[] subStr;
-            String delimeter = " "; // Разделитель
-            subStr = line.split(delimeter);
+            subStr = line.split("\\W");
             for(int i = 0; i < subStr.length; i++) {
                 listOfLines.add(subStr[i]);
             }
@@ -27,10 +27,42 @@ public class Main {
         Collections.sort(listOfLines, String.CASE_INSENSITIVE_ORDER);
 
 
-
+        System.out.println("Слова из файла в алфавитном порядке: ");
         for (String s : listOfLines){
             System.out.println(s);
         }
+
+        Map<String, Integer> statistics = new HashMap<>();
+        for (String word : listOfLines) {
+            if(!word.isEmpty()) {
+                Integer count = statistics.get(word);
+                if(count == null) {
+                    count = 0;
+                }
+                statistics.put(word, ++count);
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Статистика встречаемости слов в файле: ");
+        for(String word : statistics.keySet()) {
+            System.out.println("Слово " + "\"" + word + "\"" +" встречается в файле " + statistics.get(word) + " раз(а).");
+        }
+
+        String s = null;
+        int p = 0;
+        for (String w : statistics.keySet()) {
+            if (p < statistics.get(w)) {
+                p = statistics.get(w);
+                s = w;
+            }
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Самое повторяющееся слово: " + "\"" + s + "\"" + " (встречается " + statistics.get(s) + " раз(а))");
     }
 }
-//D:/Desktop/doc1.txt//D:/Desktop/doc1.txt
+
+
